eabdbce7f4bd5ded707af01e7f0956639d0cdd64
Brownpaper55 <michaelawuahh@gmail.com>
2023-07-28 20:41:36 -0700

    corrections

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -2,5 +2,5 @@ def raise_exception():
     try:
         raise TypeError()
     except TypeError:
-        print('exception raised')
-        raise
+        print('exception has been raised')
+        
