d20866df11d80396737d359e359381e0c3bbf26d
Brownpaper55 <michaelawuahh@gmail.com>
2023-08-04 09:40:41 -0700

    more data structures

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,6 +1,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    max_key = max(a_dictionary, key=a_dictionary.get)
-    print('{}'.format(max_key))
+    best_key = max(a_dictionary, key=a_dictionary.get)
+    print('Best score:{}'.format(best_key))
+    return ''
     
